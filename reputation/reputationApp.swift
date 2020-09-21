c6f715260c1922c7f4af3c796c0a351d29e07a2e
Shariq Hirani <shariqhirani2010@gmail.com>
2020-09-21 14:06:21 -0500

    Initial Commit

---
+++ reputation/reputationApp.swift
@@ -0,0 +1,17 @@
+//
+//  reputationApp.swift
+//  reputation
+//
+//  Created by Shariq Hirani on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct reputationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
